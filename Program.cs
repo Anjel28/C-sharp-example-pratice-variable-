03d2966cbf0e0102b661045b942f1c0268787261
Anjel28 <anjelchaudhary28@gmail.com>
2025-08-05 23:35:57 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        //WriteLine Methods
+        /* Console.WriteLine("Hello World!");
+         Console.WriteLine("I am learning C#");
+         Console.WriteLine("It is  awesome");
+         Console.WriteLine(3 + 3);*/
+
+        //Write Methods
+        /* Console.Write("Hello World !");
+         Console.Write("I will print on the same line");*/
+
+        //Variables-{String}
+        /*  string name = "John";
+          Console.WriteLine(name);*/
+        //Example
+        /*  string greeting = "hello world \n";
+          string name = "john";
+          string message;
+           message = greeting + name;
+          Console.WriteLine(message);*/
+
+        /* int myNum = 15;
+         Console.WriteLine(myNum);*/
+
+        //OR
+        /* int myNum;
+         myNum = 15;
+         Console.WriteLine(myNum);*/
+        //Example
+        /* int number = 42;
+         Console.WriteLine(number);
+         int sum = number + 10;
+         Console.WriteLine(sum);
+         int updatenumber = 100;
+         Console.WriteLine(updatenumber);*/
+
+
+
+        //{double}
+        /* double myDoubleNum = 19.99;
+           Console.WriteLine(myDoubleNum);*/
+        //Example
+        /* double price = 19.99 ;
+         double tempreture = -3.5;
+         double scientificValue = 6.023456;
+         double radius = 5.5;
+         double area = Math.PI * radius * radius;
+         Console.WriteLine(price);
+         Console.WriteLine(tempreture);
+         Console.WriteLine(scientificValue);
+         Console.WriteLine(area);*/
+
+        /*double a = 0.1;
+        double b = 0.2;
+        double sum = a + b;
+        Console.WriteLine(sum);*/
+
+        //{char}
+        /* char myLetter = 'D';
+         Console.WriteLine(myLetter);*/
+        //Example
+        /*char myChar = 'A';
+        Console.WriteLine(myChar);
+        char ch1 = 'a';
+        Console.WriteLine(ch1);
+        char ch3 = '\x0061';  // 'a'
+        Console.WriteLine(ch3);
+        char ch4 = (char)97;  // 'a'
+        Console.WriteLine(ch4);
+        char letter = 'B';
+        Console.WriteLine(letter);
+        char digit = '7';
+        Console.WriteLine(digit);
+        char lower = 'c';
+        Console.WriteLine(lower);
+        char newLine = '\n';
+        char tab = '\t';
+        char backslash = '\\';
+        char singleQuote = '\'';
+        Console.WriteLine(newLine);
+        Console.WriteLine(tab);
+        Console.WriteLine(backslash);
+        Console.WriteLine(singleQuote);*/
+
+
+        //{Bool}
+        /*bool myBool = true;
+        Console.WriteLine(myBool);*/
+        //Example
+        int x = 10;
+        int y = 20;
+        bool isXGreaterThanY = x > y; 
+           Console.WriteLine($"Is x greater than y? {isXGreaterThanY}");
+        
+    }
+}
